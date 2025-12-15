5b8a373e054726d67b8614c9ce7364f92c473ef4
Haidangke <vatcmnvo@gmail.com>
2025-12-15 08:20:11 +0700

    update code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, InjectionToken, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+export const BASE_API_URL = new InjectionToken<string>('BASE_API_URL');
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: BASE_API_URL,
+      useValue: 'https://api.example.com'
+    }
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,72 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('my-angular');
+export class App implements OnInit {
+  startDate = signal(new Date());
+  now = signal(new Date());
+
+  space = signal(0)
+
+  selectedUnit = signal('seconds');
+
+  ngOnInit(): void {
+    const startDate = new Date();
+    startDate.setFullYear(2017, 1, 12)
+    this.startDate.set(startDate);
+
+    this.updateTime();
+
+    interval(1000).subscribe(() => {
+      this.updateTime();
+    })
+  }
+
+  spaceInSeconds = computed(() => {
+    return this.space() / 1000;
+  })
+
+  spaceInMinutes = computed(() => {
+    return this.spaceInSeconds() / 60;
+  })
+
+  spaceInHours = computed(() => {
+    return this.spaceInMinutes() / 60;
+  })
+
+  spaceInDays = computed(() => {
+    return this.spaceInHours() / 24;
+  })
+
+  spaceInMonths = computed(() => {
+    const startYear = this.startDate().getFullYear();
+    const startMonth = this.startDate().getMonth();
+    const startDay = this.startDate().getDate();
+
+    const nowYear = this.now().getFullYear();
+    const nowMonth = this.now().getMonth();
+    const nowDay = this.now().getDate();
+
+    let months = (nowYear - startYear) * 12 + (nowMonth - startMonth);
+    if (nowDay < startDay) {
+      months--;
+    }
+    return months;
+  })
+
+  spanceInYears = computed(() => {
+    return Math.floor(this.spaceInMonths() / 12);
+  })
+
+  updateTime() {
+    this.now.set(new Date());
+    this.space.set(((this.now().getTime() - this.startDate().getTime())));
+  }
 }
